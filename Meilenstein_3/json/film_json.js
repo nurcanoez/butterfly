355b7686f682f6828702c2eee405b7fc7220c65a
Nurcan <oez.n@live.de>
2014-11-05 17:38:12 +0100

    meilenstein3,1223910 Nurcan Öz, 1233643 Büsra Keles, 1232010 Tülin Kol

--- Meilenstein_3/json/film_json.js
+++ Meilenstein_3/json/film_json.js
@@ -1,3 +1,77 @@
 /**
  * Created by Nurcan on 29.10.2014.
  */
+
+var film= {"filmliste":[
+    {"filmtitel":"A Clockwork Orange",
+        "regie":"Stanley",
+        "drehbuch":"Stanley Kubrick",
+        "erscheinungsjahr":"1971",
+        "genre":"Komödie"},
+
+    {"filmtitel":"Love Exposure",
+        "regie":"Haruo Umekawa",
+        "drehbuch":"Sion Sono",
+        "erscheinungsjahr":"2008",
+        "genre":"Psycho"},
+
+    {"filmtitel":"Richy Guitar",
+        "regie":"Michael Laux",
+        "drehbuch":"Michael Laux",
+        "erscheinungsjahr":"1985",
+        "genre":"Komödie"},
+
+    {"filmtitel":"Inglourious Basterds",
+        "regie":"Quentin Tarantino",
+        "drehbuch":"Quentin Tarantino",
+        "erscheinungsjahr":"2009",
+        "genre":"Komödie"},
+
+    {"filmtitel":"Pulp Fiction",
+        "regie":"Quentin Tarantino",
+        "drehbuch":"Quentin Tarantino",
+        "erscheinungsjahr":"1994",
+        "genre":"Psycho"},
+
+    {"filmtitel":"Léon - der Profi",
+        "regie":"Luc Besson",
+        "drehbuch":"Luc Besson",
+        "erscheinungsjahr":"1994",
+        "genre":"Romanze"},
+
+    {"filmtitel":"Das Schicksal ist ein mieser Verräter",
+        "regie":"Josh Boone",
+        "drehbuch":"Scott Neustädter",
+        "erscheinungsjahr":"2014",
+        "genre":"Romanze"},
+
+    {"filmtitel":"Tatsächlich ... Liebe",
+        "regie":"Richard Curtis",
+        "drehbuch":"Richard Curtis",
+        "erscheinungsjahr":"2003",
+        "genre":"Romanze"},
+
+    {"filmtitel":"Crank",
+        "regie":"Mark Neveldine",
+        "drehbuch":"Mark Neveldine",
+        "erscheinungsjahr":"2006",
+        "genre":"Komödie"},
+
+    {"filmtitel":"Snatch",
+        "regie":"Guy Ritchie",
+        "drehbuch":"Guy Ritchie",
+        "erscheinungsjahr":"2000",
+        "genre":"Komödie"},
+
+    {"filmtitel":"Trainspotting",
+        "regie":"Danny Boyle",
+        "drehbuch":"John Hodge",
+        "erscheinungsjahr":"1996",
+        "genre":"Komödie"},
+
+    {"filmtitel":"The Transporter",
+        "regie":"Louis Leterrier",
+        "drehbuch":"Luc Besson",
+        "erscheinungsjahr":"2002",
+        "genre":"Krimi"},
+]}

0fc4cffec92170d0aea5e5845bf44a46e2a80cac
TueKol <tuelinkol@gmail.com>
2014-11-17 15:01:25 +0100

    changed detail.js (Implementierung Regexund Bugfix): Meilenstein_4; Serife Kol, Büsra Keles, Nurcan Öz changed directory-Aufbau: Meilenstein_4 Serife Kol, Büsra Keles, Nurcan Öz

---
+++ Meilenstein_4/json/music_json.js
@@ -0,0 +1,65 @@
+var musicData = '{ "music" : [' +
+    '{' +
+    '"interpreter":"Farin Urlaub",' +
+    '"albumtitel":"Faszination Weltraum",' +
+    '"erscheinungsjahr": 2014,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Die &auml;rzte",' +
+    '"albumtitel":"Planet Punk",' +
+    '"erscheinungsjahr": 1995,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"The Offspring",' +
+    '"albumtitel":"Smash",' +
+    '"erscheinungsjahr": 1994,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Blink 182",' +
+    '"albumtitel":"Enema of the State",' +
+    '"erscheinungsjahr": 1999,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Rise Against",' +
+    '"albumtitel":"Siren Song of the Counter Culture",' +
+    '"erscheinungsjahr": 2004,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Vampire Weekend",' +
+    '"albumtitel":"Vampire Weekend",' +
+    '"erscheinungsjahr": 2007,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Bouncing Souls",' +
+    '"albumtitel":"How I spent my summer vacation",' +
+    '"erscheinungsjahr": 2001,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Beatsteaks",' +
+    '"albumtitel":"Living Targets",' +
+    '"erscheinungsjahr": 2002,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"Bloc Party",' +
+    '"albumtitel":"Silent Alarm",' +
+    '"erscheinungsjahr": 2005,' +
+    '"genre": "Punk" ' +
+    '},' +
+    '{' +
+    '"interpreter":"WIZO",' +
+    '"albumtitel":"Anderster",' +
+    '"erscheinungsjahr": 2004,' +
+    '"genre": "Punk" ' +
+    '}' +
+    ']}';
+
+
+var musicObject = JSON.parse(musicData);
---
+++ Meilenstein_4/json/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Nurcan on 05.11.2014.
+ */
+var text = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+var testObject = JSON.parse(text);
